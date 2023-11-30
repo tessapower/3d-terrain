7be8fb3bc4529a62f3688dc8eabc55c8f8319cf7
Tessa Power <hello@tessapower.xyz>
2023-11-30 17:36:10 -0600

    Refactor Terrain source files to conform to Google C++ style guidelines

--- src/terrain/terrain_model.cpp
+++ src/terrain/terrain_model.cpp
@@ -1,107 +1,119 @@
-#pragma once
-
 #include "terrain/terrain_model.hpp"
-#include "utils/perlin_noise.hpp"
+
 #include <glm/gtc/type_ptr.hpp>
 
-void terrain_model::createTerrain(bool perlin) {
-	cgra::mesh_builder mb;
-	cgra::mesh_vertex mv;
+#include "utils/perlin_noise.hpp"
 
-	Perlin terrain = Perlin(seed, octaves, lacunarity, persistence, repeat);
+auto terrain_model::create_terrain(bool perlin) -> void {
+  cgra::mesh_builder mb;
+  cgra::mesh_vertex mv;
 
-	// Calculate the total width and length of the grid
-	float totalWidth = gridSize * spacing;
-	float totalLength = gridSize * spacing;
+  auto terrain =
+      Perlin(m_seed, m_octaves, m_lacunarity, m_persistence, m_repeat);
 
-	// Calculate the offset to center the grid
-	float xOffset = -totalWidth / 2.0f;
-	float zOffset = -totalLength / 2.0f;
+  // Calculate the total width and length of the grid
+  float total_width = m_spacing_ * static_cast<float>(m_grid_size);
+  float total_length = m_spacing_ * static_cast<float>(m_grid_size);
 
-	// Create a 2D array to store adjacent faces for each vertex
-	adjacent_faces = std::vector<std::vector<int>>((gridSize + 1) * (gridSize + 1));
+  // Calculate the offset to center the grid
+  float x_offset = -total_width / 2.0f;
+  float z_offset = -total_length / 2.0f;
 
-	// Generate the grid vertices
-	for (int i = 0; i <= gridSize; ++i) {
-		for (int j = 0; j <= gridSize; ++j) {
-			// Calculate vertex position (x, y, z) with increased spacing and centered
-			float x = static_cast<float>(i) * spacing + xOffset;
-			float z = static_cast<float>(j) * spacing + zOffset;
-			// whether to use perlin or not
-			float y = perlin ? (terrain.perlin(x, 0.0, z) * height) - (height / 2) : 0.0f;
+  // Create a 2D array to store adjacent faces for each vertex
+  m_adjacent_faces =
+      std::vector<std::vector<int>>((m_grid_size + 1) * (m_grid_size + 1));
 
-			mv.pos = { x, y, z };
+  // Generate the grid vertices
+  for (int i = 0; i <= m_grid_size; ++i) {
+    for (int j = 0; j <= m_grid_size; ++j) {
+      // Calculate vertex position (x, y, z) with increased spacing and centered
+      float x = static_cast<float>(i) * m_spacing_ + x_offset;
+      float z = static_cast<float>(j) * m_spacing_ + z_offset;
+      // whether to use perlin or not
+      float y = perlin ? (terrain.perlin(x, 0.0, z) * m_height) - (m_height / 2)
+                       : 0.0f;
 
-			// Normal vectors for flat ground (facing up)
-			mv.norm = { 0.0f, 1.0f, 0.0f };
+      mv.pos = {x, y, z};
 
-			// Texture coordinates (u, v) can be set as desired
-			mv.uv = { static_cast<float>(i) * 10 / gridSize, static_cast<float>(j) * 10 / gridSize };
+      // Normal vectors for flat ground (facing up)
+      mv.norm = {0.0f, 1.0f, 0.0f};
 
-			mb.push_vertex(mv);
-		}
-	}
+      // Texture coordinates (u, v) can be set as desired
+      mv.uv = {static_cast<float>(i) * 10.0f / static_cast<float>(m_grid_size),
+               static_cast<float>(j) * 10.0f / static_cast<float>(m_grid_size)};
 
-	// Generate triangle indices to create the grid
-	for (int i = 0; i < gridSize; ++i) {
-		for (int j = 0; j < gridSize; ++j) {
-			int k1 = i * (gridSize + 1) + j;
-			int k2 = k1 + 1;
-			int k3 = (i + 1) * (gridSize + 1) + j;
-			int k4 = k3 + 1;
+      mb.push_vertex(mv);
+    }
+  }
 
-			// First triangle
-			mb.push_index(k1);
-			mb.push_index(k2);
-			mb.push_index(k3);
+  // Generate triangle indices to create the grid
+  for (int i = 0; i < m_grid_size; ++i) {
+    for (int j = 0; j < m_grid_size; ++j) {
+      int k1 = i * (m_grid_size + 1) + j;
+      int k2 = k1 + 1;
+      int k3 = (i + 1) * (m_grid_size + 1) + j;
+      int k4 = k3 + 1;
 
-			// Second triangle
-			mb.push_index(k2);
-			mb.push_index(k4);
-			mb.push_index(k3);
+      // First triangle
+      mb.push_index(k1);
+      mb.push_index(k2);
+      mb.push_index(k3);
 
-			adjacent_faces[k1].push_back(k1);
-			adjacent_faces[k1].push_back(k2);
-			adjacent_faces[k1].push_back(k3);
-			adjacent_faces[k2].push_back(k1);
-			adjacent_faces[k2].push_back(k2);
-			adjacent_faces[k2].push_back(k3);
-			adjacent_faces[k3].push_back(k1);
-			adjacent_faces[k3].push_back(k2);
-			adjacent_faces[k3].push_back(k3);
+      // Second triangle
+      mb.push_index(k2);
+      mb.push_index(k4);
+      mb.push_index(k3);
 
-			adjacent_faces[k2].push_back(k2);
-			adjacent_faces[k2].push_back(k4);
-			adjacent_faces[k2].push_back(k3);
-			adjacent_faces[k4].push_back(k2);
-			adjacent_faces[k4].push_back(k4);
-			adjacent_faces[k4].push_back(k3);
-			adjacent_faces[k3].push_back(k2);
-			adjacent_faces[k3].push_back(k4);
-			adjacent_faces[k3].push_back(k3);
-		}
-	}
+      m_adjacent_faces[k1].push_back(k1);
+      m_adjacent_faces[k1].push_back(k2);
+      m_adjacent_faces[k1].push_back(k3);
+      m_adjacent_faces[k2].push_back(k1);
+      m_adjacent_faces[k2].push_back(k2);
+      m_adjacent_faces[k2].push_back(k3);
+      m_adjacent_faces[k3].push_back(k1);
+      m_adjacent_faces[k3].push_back(k2);
+      m_adjacent_faces[k3].push_back(k3);
 
-	builder = mb;
-	mesh = mb.build();
+      m_adjacent_faces[k2].push_back(k2);
+      m_adjacent_faces[k2].push_back(k4);
+      m_adjacent_faces[k2].push_back(k3);
+      m_adjacent_faces[k4].push_back(k2);
+      m_adjacent_faces[k4].push_back(k4);
+      m_adjacent_faces[k4].push_back(k3);
+      m_adjacent_faces[k3].push_back(k2);
+      m_adjacent_faces[k3].push_back(k4);
+      m_adjacent_faces[k3].push_back(k3);
+    }
+  }
+
+  m_builder = mb;
+  m_mesh = mb.build();
 }
 
-void terrain_model::draw(const glm::mat4& view, const glm::mat4& proj) {
-	glUseProgram(shader);
-	// Set uniform values
-	glUniform3f(glGetUniformLocation(shader, "uCenter"), selectedPoint.pos.x, selectedPoint.pos.y, selectedPoint.pos.z); // Set the center point
-	glUniform1f(glGetUniformLocation(shader, "uRadius"), m_radius); // Set the selection radius
-	glUniform1f(glGetUniformLocation(shader, "uHeightChange1"), m_heightChange1);
-	glUniform1f(glGetUniformLocation(shader, "uHeightChange2"), m_heightChange2);
-	glUniform1iv(glGetUniformLocation(shader, "uTex"), 1, &m_tex);
-	glUniform1f(glGetUniformLocation(shader, "uHeightScale"), m_heightScale);
-	glUniform1iv(glGetUniformLocation(shader, "uType"), 1, &m_type);
-	glUniformMatrix4fv(glGetUniformLocation(shader, "uProjectionMatrix"), 1, false, value_ptr(proj));
-	glUniformMatrix4fv(glGetUniformLocation(shader, "uModelViewMatrix"), 1, false, value_ptr(view));
+auto terrain_model::draw(const glm::mat4& view,
+                         const glm::mat4& projection) const -> void {
+  glUseProgram(m_shader);
+  // Set uniform values
+  glUniform3f(glGetUniformLocation(m_shader, "uCenter"), m_selected_point.pos.x,
+              m_selected_point.pos.y,
+              m_selected_point.pos.z);  // Set the center point
+  glUniform1f(glGetUniformLocation(m_shader, "uRadius"),
+              m_radius);  // Set the selection radius
+  glUniform1f(glGetUniformLocation(m_shader, "uHeightChange1"),
+              m_height_change1);
+  glUniform1f(glGetUniformLocation(m_shader, "uHeightChange2"),
+              m_height_change2);
+  glUniform1iv(glGetUniformLocation(m_shader, "uTex"), 1, &m_tex);
+  glUniform1f(glGetUniformLocation(m_shader, "uHeightScale"), m_height_scale);
+  glUniform1iv(glGetUniformLocation(m_shader, "uType"), 1, &m_type_);
+  glUniformMatrix4fv(glGetUniformLocation(m_shader, "uProjectionMatrix"), 1,
+                     false, value_ptr(projection));
+  glUniformMatrix4fv(glGetUniformLocation(m_shader, "uModelViewMatrix"), 1,
+                     false, value_ptr(view));
 
-	mesh.draw(); // draw
+  m_mesh.draw();  // draw
 
-	// reset for other objects
-	int reset = -1;
-	glUniform1iv(glGetUniformLocation(shader, "uType"), 1, &reset);
+  // reset for other objects
+  constexpr int reset = -1;
+  glUniform1iv(glGetUniformLocation(m_shader, "uType"), 1, &reset);
 }
--- src/terrain/terrain_model.hpp
+++ src/terrain/terrain_model.hpp
@@ -1,53 +1,47 @@
 #pragma once
 
-// glm
 #include <glm/glm.hpp>
 
-// project
-#include "utils/opengl.hpp"
 #include "cgra/cgra_mesh.hpp"
+#include "utils/opengl.hpp"
 
+/// Code Author: Shekinah Pratap
 
-/*
-
-This is a C++ class for creating and rendering a terrain model.
-
-Code Author: Shekinah Pratap
-
-*/
-
+/**
+ * \brief Class for creating and rendering a terrain model
+ */
 class terrain_model {
-public:
-	GLuint shader;
-	cgra::gl_mesh mesh;
-	cgra::mesh_builder builder;
+ public:
+  GLuint m_shader;
+  cgra::gl_mesh m_mesh;
+  cgra::mesh_builder m_builder;
 
-	std::vector<std::vector<int>> adjacent_faces;
+  std::vector<std::vector<int>> m_adjacent_faces;
 
-	// variables
-	int m_tex = 1;
-	cgra::mesh_vertex selectedPoint;
-	float m_radius = 25;
-	bool m_isBump = true;
-	float m_strength = 7;
-	float m_heightChange1 = 7;
-	float m_heightChange2 = -25;
-	float m_heightScale = 1;
-	int gridSize = 200;
+  // variables
+  int m_tex = 1;
+  cgra::mesh_vertex m_selected_point;
+  float m_radius = 25;
+  bool m_is_bump = true;
+  float m_strength = 7;
+  float m_height_change1 = 7;
+  float m_height_change2 = -25;
+  float m_height_scale = 1;
+  int m_grid_size = 200;
 
-	// noise variables
-	unsigned int seed = 0;
-	unsigned int octaves = 5;
-	float lacunarity = 2.0f;
-	float persistence = 0.5f;
-	unsigned int repeat = 3;
-	float height = 200.0f;
+  // noise variables
+  unsigned int m_seed = 0;
+  unsigned int m_octaves = 5;
+  float m_lacunarity = 2.0f;
+  float m_persistence = 0.5f;
+  unsigned int m_repeat = 3;
+  float m_height = 200.0f;
 
-	terrain_model() { }
-	void draw(const glm::mat4& view, const glm::mat4& proj);
-	void createTerrain(bool perlin);
+  terrain_model() = default;
+  auto draw(const glm::mat4& view, const glm::mat4& projection) const -> void;
+  void create_terrain(bool perlin);
 
-private:
-	float spacing = 5.0f;  // spacing between grid points
-	int m_type = 0;
+ private:
+  float m_spacing_ = 5.0f;  // spacing between grid points
+  int m_type_ = 0;
 };
