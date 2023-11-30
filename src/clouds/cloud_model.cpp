895dea4dacd186f7c669295d25b3d88f3f1ea23c
Tessa Power <hello@tessapower.xyz>
2023-11-30 15:32:17 -0600

    Refactor Cloud source files to conform to Google C++ style guidelines

--- src/clouds/cloud_model.cpp
+++ src/clouds/cloud_model.cpp
@@ -1,65 +1,56 @@
-#pragma once
+#include "cloud_model.hpp"
 
-// glm
-#include <glm/gtc/constants.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-// project
-#include "application.hpp"
-#include "cgra/cgra_geometry.hpp"
-#include "cgra/cgra_gui.hpp"
-#include "cgra/cgra_image.hpp"
-#include "cgra/cgra_shader.hpp"
+#include "PerlinNoise.hpp"
 #include "cgra/cgra_wavefront.hpp"
 #include "mesh/simplified_mesh.hpp"
 
-#include "cloud_model.hpp"
-#include "PerlinNoise.hpp"
-
-using namespace std;
-using namespace cgra;
-using namespace glm;
-
-float cloud_falloff(float x) { return 1.f - ((x - 1.f) * (x - 1.f)); }
-
-void cloud_model::simulate() {
-
-	// Generate volumeric data
-	cloudData = vector(size.x, vector(size.y, vector(size.z, 1.0f)));
-
-	const siv::PerlinNoise perlin{ 0 };
-
-	for (int x = 0; x < size.x; x++) {
-		for (int y = 0; y < size.y; y++) {
-			for (int z = 0; z < size.z; z++) {
-				double noise = perlin.octave3D_01((x / noiseScale.x), y / noiseScale.y, z / noiseScale.z, 3);
-
-				// Fade out at top and bottom
-				if (y > size.y - fadeOutRange) {
-					noise *= cloud_falloff(1. - (y - (size.y - fadeOutRange)) / fadeOutRange);
-				}
-				else if (y < fadeOutRange) {
-					noise *= cloud_falloff(y / fadeOutRange);
-				}
-
-				// 1 is not solid, 0 is solid
-				cloudData[x][y][z] = noise > cloudThreshold ? 0.0 : 1.0;
-			}
-		}
-	}
-
-	std::cout << "Finished building noise map\n";
-
-	mesh.G = cloudData;
-	mesh.topRight = size;
-	mesh.bottomLeft = vec3(0.0f);
-	mesh.voxelEdgeLength = 1.0;
-	mesh.build();
+auto cloud_falloff(const float x) -> float {
+  return 1.0f - ((x - 1.0f) * (x - 1.0f));
 }
 
-void cloud_model::draw(const glm::mat4& view, const glm::mat4 proj) {
-	mat4 viewmodel = glm::scale(view, vec3(5.f));
-	viewmodel = glm::translate(viewmodel, vec3(size.x / -2.f, 40.f, size.z / -2.f));
-	mesh.shader = shader;
-	mesh.draw(viewmodel, proj); // draw
+auto cloud_model::simulate() -> void {
+  // Generate volumetric data
+  cloud_data =
+      std::vector(size.x, std::vector(size.y, std::vector(size.z, 1.0f)));
+
+  const siv::PerlinNoise perlin{0};
+
+  for (int x = 0; x < size.x; x++) {
+    for (int y = 0; y < size.y; y++) {
+      for (int z = 0; z < size.z; z++) {
+        double noise = perlin.octave3D_01(
+            (x / noise_scale.x), y / noise_scale.y, z / noise_scale.z, 3);
+
+        // Fade out at top and bottom
+        if (y > size.y - fade_out_range) {
+          noise *= cloud_falloff(1.0 - (y - (size.y - fade_out_range)) /
+                                           fade_out_range);
+        } else if (y < fade_out_range) {
+          noise *= cloud_falloff(y / fade_out_range);
+        }
+
+        // 1 is not solid, 0 is solid
+        cloud_data[x][y][z] = noise > cloud_threshold ? 0.0f : 1.0f;
+      }
+    }
+  }
+
+  std::cout << "Finished building noise map\n";
+
+  mesh.m_grid = cloud_data;
+  mesh.m_bb_top_right = size;
+  mesh.m_bb_bottom_left = glm::vec3(0.0f);
+  mesh.m_voxel_edge_length = 1.0f;
+  mesh.build();
+}
+
+auto cloud_model::draw(const glm::mat4& view, const glm::mat4& projection)
+    -> void {
+  glm::mat4 viewmodel = glm::scale(view, glm::vec3(5.0f));
+  viewmodel = glm::translate(viewmodel,
+                             glm::vec3(size.x / -2.0f, 40.0f, size.z / -2.0f));
+  mesh.m_shader = m_shader;
+  mesh.draw(viewmodel, projection);
 }
--- src/clouds/cloud_model.hpp
+++ src/clouds/cloud_model.hpp
@@ -1,39 +1,28 @@
 #pragma once
 
-// glm
-#include <glm/gtc/constants.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-// project
-#include "application.hpp"
-#include "cgra/cgra_geometry.hpp"
 #include "cgra/cgra_gui.hpp"
 #include "cgra/cgra_image.hpp"
-#include "cgra/cgra_shader.hpp"
-#include "cgra/cgra_wavefront.hpp"
 #include "mesh/simplified_mesh.hpp"
 
-using namespace std;
-using namespace glm;
-using namespace cgra;
-
 class cloud_model {
-public:
-	GLuint shader = 0;
-	glm::vec3 color{0.7};
-	glm::mat4 modelTransform{1.0};
-	GLuint texture;
+ public:
+  GLuint m_shader = 0;
+  glm::vec3 m_color{0.7f};
+  glm::mat4 m_model_transform{1.0f};
+  GLuint m_texture;
 
-	vec3 noiseScale{ 80.f, 60.f, 80.f };
-	float cloudThreshold = 0.66;
-	double voxelEdgeLength = 1.0;
-	vec3 size{ 300.f, 30.f, 300.f };
-	float fadeOutRange = 6.;
+  glm::vec3 noise_scale{80.0f, 60.0f, 80.0f};
+  float cloud_threshold = 0.66f;
+  double voxel_edge_length = 1.0;
+  glm::vec3 size{300.f, 30.f, 300.f};
+  float fade_out_range = 6.0f;
 
-	vector<vector<vector<float>>> cloudData;
+  std::vector<std::vector<std::vector<float>>> cloud_data;
 
-	simplified_mesh mesh;
+  simplified_mesh mesh;
 
-	void simulate();
-	void draw(const glm::mat4& view, const glm::mat4 proj);
+  auto simulate() -> void;
+  auto draw(const glm::mat4& view, const glm::mat4& projection) -> void;
 };
